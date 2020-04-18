676d8c9c3ecf72605287851f21d8e3b989c5c0ff
mmerabet <mhmd.merabet@gmail.com>
2020-04-18 18:11:18 +0700

    Ajouts des Endpoints info, health, metrics, httptrace

--- src/main/java/com/mentor/safetynetalerts/SafetynetalertsApplication.java
+++ src/main/java/com/mentor/safetynetalerts/SafetynetalertsApplication.java
@@ -1,13 +1,20 @@
 package com.mentor.safetynetalerts;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SafetynetalertsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SafetynetalertsApplication.class, args);
+	}
+	
+	@Bean
+	InMemoryHttpTraceRepository httptrace(){
+		return new InMemoryHttpTraceRepository();
 	}
 
 }
